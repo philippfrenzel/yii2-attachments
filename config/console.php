78ae2084f14679797328d39c8dd5172eb07073d9
Damian Gomez <racksoft@gmail.com>
2016-10-27 16:20:35 +0200

    Moving files to make all work

--- config/console.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'controllerMap' => [
-        'file' => [
-            'class' => 'yii\console\controllers\MigrateController',
-            'migrationPath' => '@file/migrations'
-        ],
-    ],
-];
